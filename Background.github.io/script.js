70b6af80ff492ee51a71b899a9e796eb8a3c9c60
Raghebbenamor <39734862+Raghebbenamor@users.noreply.github.com>
2018-05-31 12:56:47 +0100

    background

---
+++ Background.github.io/script.js
@@ -0,0 +1,20 @@
+var css = document.querySelector("h3") ;
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var body = document.getElementById("gradiant");
+
+function SetGradient (){
+  body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+    
+    
+css.textContent = body.style.background + ";" ;
+}    
+    
+color1.addEventListener("input" , SetGradient);
+
+color2.addEventListener("input" , SetGradient);
